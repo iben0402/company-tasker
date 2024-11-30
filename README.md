b0faf8fddb0003ab723eda63d5636f9df670d88e
Iwona Bendig <iwona.bbbb@gmail.com>
2024-11-30 11:00:00 +0100

    fix sql config errors

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # company-tasker
 small software for companies working in offices to manage their tasks
+
+## how to run:
+1. docker-compose up -d - starts sql server and creates initial data
--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -2,22 +2,21 @@ CREATE DATABASE IF NOT EXISTS tasker;
 USE tasker;
 
 -- ROLE
-CREATE TABLE IF NOT EXISTS roles {
+CREATE TABLE IF NOT EXISTS roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
-}
+);
 
 -- TEAM
-CREATE TABLE IF NOT EXISTS teams {
+CREATE TABLE IF NOT EXISTS teams (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    owner_id INT NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES users(id)
-}
+    owner_id INT NOT NULL
+);
 
 -- USER
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -25,25 +24,27 @@ CREATE TABLE IF NOT EXISTS users {
     team_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (team_id) REFERENCES teams(id)
-}
+);
+
+ALTER TABLE teams ADD FOREIGN KEY (owner_id) REFERENCES users(id);
 
 -- PROJECT
-CREATE TABLE IF NOT EXISTS projects {
+CREATE TABLE IF NOT EXISTS projects (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     description TEXT
-}
+);
 
 -- PROJECT_TEAM (MANY TO MANY)
-CREATE TABLE IF NOT EXISTS project_team {
+CREATE TABLE IF NOT EXISTS project_team (
     project_id INT,
     team_id INT,
     FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (team_id) REFERENCES teams(id)
-}
+);
 
 -- TASK
-CREATE TABLE IF NOT EXISTS tasks {
+CREATE TABLE IF NOT EXISTS tasks (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     description TEXT,
@@ -55,10 +56,10 @@ CREATE TABLE IF NOT EXISTS tasks {
     status VARCHAR(255),
     FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (assignee_id) REFERENCES users(id)
-}
+);
 
 -- COMMENT
-CREATE TABLE IF NOT EXISTS comments {
+CREATE TABLE IF NOT EXISTS comments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     content TEXT,
     task_id INT,
@@ -66,10 +67,10 @@ CREATE TABLE IF NOT EXISTS comments {
     create_date DATE,
     FOREIGN KEY (task_id) REFERENCES tasks(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
-}
+);
 
 -- ATTACHMENT
-CREATE TABLE IF NOT EXISTS attachments {
+CREATE TABLE IF NOT EXISTS attachments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     path VARCHAR(255) NOT NULL,
@@ -78,7 +79,7 @@ CREATE TABLE IF NOT EXISTS attachments {
     create_date DATE,
     FOREIGN KEY (task_id) REFERENCES tasks(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
-}
+);
 
 -- INITIAL DATA
 INSERT INTO roles (name) VALUES ('admin');
